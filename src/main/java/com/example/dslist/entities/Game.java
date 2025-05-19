9bb199f7ff8cbda1b1c362d697756d9e318dc822
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-05-19 16:59:46 -0300

    Game seed

--- src/main/java/com/example/dslist/entities/Game.java
+++ src/main/java/com/example/dslist/entities/Game.java
@@ -22,7 +22,11 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT") 
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT") //JPA gera o campo como texto e nao como varchar de no max 255 caracteres
     private String longDescription;
 
     public Game () {
